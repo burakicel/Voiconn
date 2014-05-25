2da5294a29b8a65883aadc4c1f9246561a10ff97
burakicel <burakicel96@gmail.com>
2014-05-25 02:08:31 -0400

    added a model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 RubyRailsSample::Application.routes.draw do
-  root 'welcome#index'
+  root 'welcome#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
