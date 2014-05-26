0068f81dfed722999b69deeb5df1008e6605b955
burakicel <burakicel96@gmail.com>
2014-05-25 22:48:28 -0400

    database test

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -15,6 +15,16 @@ class Owner
 	def companies
 		conn = PGconn.connect("ec2-54-225-239-184.compute-1.amazonaws.com", 5432, '', '', "de6mutapp7fbf", "wbqmgeaxrzvyzr", "7kZ0Wtu6FyFjgHy7MEG4k2y9Ho")
 		res  = conn.exec('SELECT last_name FROM employees')
+		
+		ary = Array.new
+		counter = 0
+
+		while counter < 6  do
+   			ary.push(res[counter]["last_name"])
+   			counter += 1
+		end
+
+		bea = ary
 	end
 
 end
