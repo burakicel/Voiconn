a097b26b7268f4df3444538729719231da91afcd
burakicel <burakicel96@gmail.com>
2014-05-25 21:25:38 -0400

    database test

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -12,7 +12,7 @@ class Owner
 		conn = PGconn.connect("ec2-54-225-239-184.compute-1.amazonaws.com", 5432, '', '', "de6mutapp7fbf", "wbqmgeaxrzvyzr", "7kZ0Wtu6FyFjgHy7MEG4k2y9Ho")
 		res  = conn.exec('SELECT 1 AS a, 2 AS b, NULL AS c')
 		description = 'Voiconn is a stock trading simulation that will train new investors.'
-		hey = res[1]
+		hey = res(0,2)
 	end
 
 end
