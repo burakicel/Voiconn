85c6c01b97078c3a84ce9b6cda140b5be9c0a60b
burakicel <burakicel96@gmail.com>
2014-05-25 20:17:02 -0400

    modified database structure

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -13,15 +13,4 @@ class Owner
 		description = 'Voiconn is a stock trading simulation that will train new investors.'
 	end
 
-	def database_exists?(database_name)
-  		conn = PG.connect(:dbname => 'postgres')
-  		res = conn.exec("SELECT datname FROM pg_database").values.flatten
-  		res.include?(database_name)
-	end
-
-	def check
-		check = database_exists?('yellow')
-	end
-
-
 end
